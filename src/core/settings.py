964091636b5f63514c4709110e82690af2735a24
San <Sanfo599@gmail.com>
2024-06-24 12:18:51 +0100

    Settings.py populated

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,54 +10,121 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+# # Configure Gmail for Django Emails: https://www.codingforentrepreneurs.com/blog/sending-email-in-django-from-gmail/
+
+# # Email config
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = config("EMAIL_HOST", cast=str, default="smtp.gmail.com")
+# EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587") # Recommended
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)  # Use EMAIL_PORT 587 for TLS
+# EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)  # Use MAIL_PORT 465 for SSL
+# ADMIN_USER_NAME=config("ADMIN_USER_NAME", default="Admin user")
+# ADMIN_USER_EMAIL=config("ADMIN_USER_EMAIL", default=None)
+
+
+# MANAGERS=[]
+# ADMINS=[]
+# if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+#     # 500 errors are emailed to these users
+#     ADMINS +=[
+#         (f'{ADMIN_USER_NAME}', f'{ADMIN_USER_EMAIL}')
+#     ]
+#     MANAGERS=ADMINS
+
+
+
+
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ybq0@po0-f2j3fzn@alqktqtl*$ka*i6+1zoh1!wx8&-_luy5&'
+SECRET_KEY = config("DJANGO_SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-ALLOWED_HOSTS = [".gammadelta.ie"]
-
-DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+BASE_URL = config("BASE_URL", default=None)
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
 if DEBUG:
     ALLOWED_HOSTS += [
         "127.0.0.1",
-        "localhost",
+        "localhost"
     ]
+
+
+
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # django-apps
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'visits',
+    # my-apps
+#    "commando",
+#    "customers",
+#    "profiles",
+#    "subscriptions",
+    "visits",
+
+    # third-party-apps
+#    "allauth_ui",
+#    'allauth',
+#    'allauth.account',
+#    'allauth.socialaccount',
+#    'allauth.socialaccount.providers.github',
+#    "widget_tweaks",
 ]
+
+
+
+
+
+
+
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    # "allauth.account.middleware.AccountMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+
+
+
 ROOT_URLCONF = 'core.urls'
+
+
+
+
 
 TEMPLATES = [
     {
@@ -75,7 +142,14 @@ TEMPLATES = [
     },
 ]
 
+
+
+
+
 WSGI_APPLICATION = 'core.wsgi.application'
+
+
+
 
 
 # Database
@@ -87,6 +161,54 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
+# DATABASE_URL = config("DATABASE_URL", default=None)
+
+# if DATABASE_URL is not None:
+#     import dj_database_url
+#     DATABASES = {
+#         "default": dj_database_url.config(
+#             default=DATABASE_URL,
+#             conn_max_age=CONN_MAX_AGE,
+#             conn_health_checks=True,
+#         )
+#     }
+
+
+
+
+
+
+
+
+# Add these at the top of your settings.py
+# from os import getenv
+# from dotenv import load_dotenv
+
+# Replace the DATABASES section of your settings.py with this
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': config('PGDATABASE'),
+#     'USER': config('PGUSER'),
+#     'PASSWORD': config('PGPASSWORD'),
+#     'HOST': config('PGHOST'),
+#     'PORT': config('PGPORT', 5432),
+#     'OPTIONS': {
+#       'sslmode': 'require',
+#     },
+#   }
+# }
+
+
+
+
+
+
+
 
 
 # Password validation
@@ -108,6 +230,45 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
+
+
+
+
+# # Django Allauth Config 
+# LOGIN_REDIRECT_URL = "/"
+# ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+# ACCOUNT_EMAIL_VERIFICATION="mandatory"
+# ACCOUNT_EMAIL_SUBJECT_PREFIX="[CFE] "
+# ACCOUNT_EMAIL_REQUIRED=True
+
+# AUTHENTICATION_BACKENDS = [
+#     # ...
+#     # Needed to login by username in Django admin, regardless of `allauth`
+#     'django.contrib.auth.backends.ModelBackend',
+
+#     # `allauth` specific authentication methods, such as login by email
+#     'allauth.account.auth_backends.AuthenticationBackend',
+#     # ...
+# ]
+
+# SOCIALACCOUNT_PROVIDERS = {
+#     "github": {
+#         "VERIFIED_EMAIL": True
+#     }
+# }
+
+
+
+
+
+
+
+
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -120,10 +281,48 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+
+
+
+# STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+# STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+# STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# # source(s) for python manage.py collectstatic 
+# STATICFILES_DIRS = [
+#     STATICFILES_BASE_DIR
+# ]
+
+# # output for python manage.py collectstatic 
+# # local cdn
+# STATIC_ROOT = BASE_DIR / "local-cdn"
+
+# # < Django 4.2
+# # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
+
+
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
