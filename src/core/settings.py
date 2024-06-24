5b12525bfee09e3bab259d5bfb3997f3e91f8a82
San <Sanfo599@gmail.com>
2024-06-24 14:19:46 +0100

    Tidy up for HTML page

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,9 +56,14 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+
+
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 BASE_URL = config("BASE_URL", default=None)
+
+
 ALLOWED_HOSTS = [
+    ".gammadelta.ie",
     ".railway.app" # https://saas.prod.railway.app
 ]
 if DEBUG:
